ae546599c0970b6061bf0e5440f22085fff3fe57
Amman Sood <392075+asood92@users.noreply.github.com>
2021-01-10 16:45:48 -0500

    rewriting with classes

--- app.py
+++ app.py
@@ -11,42 +11,83 @@ app = Flask(__name__)
 # pp = PrettyPrinter(indent=4)
 
 
-@app.route("/<query>", methods=["GET"])
-def search(query):
-    searchterm = str(query)
-    movieid = requests.get(
-        f"https://api.themoviedb.org/3/search/movie?api_key={API_KEY}&query={searchterm}&page=1&append_to_response=videos,images"
-    )
-    # print(movieid)
+class MovieDB:
+    API_KEY = "833dd9a67ff574327361b76a61d6cb13"
 
-    # print()
-
-    # pp.pprint(movieid)
-
-    tempid = movieid.json()
-
-    print()
-
-    # pp.pprint(tempid["results"])
-    print()
-    results = tempid["results"]
-    overview = results[0]
-    pp.pprint(overview["overview"])
-
-    # movieinfo = requests.get(
-    #     f"https://api.themoviedb.org/3/movie/{movieid}/credits?api_key=833dd9a67ff574327361b76a61d6cb13&language=en-US"
-    # )
-    # print(movieinfo)
-    # tempinfo = movieinfo.json()
-    # print(tempinfo)
-
-    context = {
-        # "movieinfo": tempinfo,
-        "overview": overview,
-    }
-    return render_template("page.html", **context)
+    def __init__(self):
+        self.base_url = "https://api.themoviedb.org/3/"
 
 
-# movieinfo = requests.get(
-#     f"https://api.themoviedb.org/3/search/movie?api_key={API_KEY}&query=what%20we%20do%20in%20the%20shadows&page=1"
-# )
+class Search(MovieDB):
+    def movie(self, query):
+
+
+#     movieinfo = requests.get(
+#         f"https://api.themoviedb.org/3/movie/{id}?api_key={API_KEY}&language=en-US&append_to_response=videos%2Cimage
+
+    def getMovieID(self, query):
+
+        query.replace(" ", "%")
+        query.replace("'", "%27")
+
+        path = f"/search/movie?{API_KEY}&query={query}&page=1"
+
+        movieid = requests.get(self.base_url + path).json()
+
+        movieid = movieid["results"][0]["id"]
+
+        return movieid
+
+
+# @app.route("/<query>", methods=["GET"])
+# def search(query):
+#     searchterm = str(query)
+#     movieid = requests.get(
+#         f"https://api.themoviedb.org/3/search/movie?api_key={API_KEY}&query={searchterm}&page=1"
+#     )
+
+#     tempid = movieid.json()
+
+#     id = tempid["results"][0]["id"]
+
+#     movieinfo = requests.get(
+#         f"https://api.themoviedb.org/3/movie/{id}?api_key={API_KEY}&language=en-US&append_to_response=videos%2Cimages"
+#     )
+
+#     imagepath = requests.get(
+#         f"https://api.themoviedb.org/3/configuration?api_key=833dd9a67ff574327361b76a61d6cb13"
+#     ).json()
+
+#     movieinfo = movieinfo.json()
+
+#     pp.pprint(movieinfo)
+
+#     print()
+
+#     # trailer = requests.get(f"http")
+
+#     # pp.pprint(tempid["results"])
+#     print()
+#     results = tempid["results"]
+#     overview = results[0]
+#     # pp.pprint(overview["overview"])
+#     # print()
+#     # pp.pprint(overview)
+
+#     # movieinfo = requests.get(
+#     #     f"https://api.themoviedb.org/3/movie/{movieid}/credits?api_key=833dd9a67ff574327361b76a61d6cb13&language=en-US"
+#     # )
+#     # print(movieinfo)
+#     # tempinfo = movieinfo.json()
+#     # print(tempinfo)
+
+#     context = {
+#         # "movieinfo": tempinfo,
+#         "overview": overview,
+#     }
+#     return render_template("page.html", **context)
+
+
+# # movieinfo = requests.get(
+# #     f"https://api.themoviedb.org/3/search/movie?api_key={API_KEY}&query=what%20we%20do%20in%20the%20shadows&page=1"
+# # )
