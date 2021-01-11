bea9534b9c1dbd7ef82ee157f917c159b95f3b57
Amman Sood <392075+asood92@users.noreply.github.com>
2021-01-11 13:33:36 -0500

    external ratings working

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ import requests
 from flask import Flask
 from pprint import PrettyPrinter
 from urllib.parse import urlencode
+from collections import ChainMap
 
-API_KEY = "833dd9a67ff574327361b76a61d6cb13"
+TMDB_API_KEY = "833dd9a67ff574327361b76a61d6cb13"
+OMDB_API_KEY = "e91e99e3"
 
 pp = PrettyPrinter(indent=4)
 
@@ -12,36 +14,50 @@ app = Flask(__name__)
 
 
 @app.route("/<query>", methods=["GET"])
-# def sanitizeQuery(query):
-#     safeQuery = {"query": {query}}
-#     urlencode(safeQuery)
-#     return safeQuery
+def sanitizeQuery(query):
+    safeQuery = {"query": {query}}
+    urlencode(safeQuery)
+    return safeQuery
 
 
 def getMovieInfo(query):
     id = getMovieID(query)
 
-    path = f"https://api.themoviedb.org/3/movie/{id}?api_key={API_KEY}&language=en-US&append_to_response=videos%2Cimage"
+    path = f"https://api.themoviedb.org/3/movie/{id}?api_key={TMDB_API_KEY}&language=en-US&append_to_response=videos%2Cimage"
     movieInfo = requests.get(path).json()
     # posterBase = requests.get(
     #     f"https://api.themoviedb.org/3/configuration?api_key=833dd9a67ff574327361b76a61d6cb13"
     # ).json()
-
+    ratings = getRatings(movieInfo["imdb_id"])
     context = {
-        "movieGenre": movieInfo["genres"][0]["name"],
-        "movieOverview": movieInfo["overview"],
+        "movieInfo": movieInfo,
+        # "movieGenre": movieInfo["genres"][0]["name"],
+        # "movieTitle": movieInfo["original_title"],
+        # "movieOverview": movieInfo["overview"],
         "moviePoster": f"https://image.tmdb.org/t/p/w500" + movieInfo["poster_path"],
         "movieTrailer": f"https://youtube.com/embed/"
         + str(movieInfo["videos"]["results"][0]["key"]),
+        "ratings": ratings,
     }
     return render_template("page.html", **context)
 
 
 def getMovieID(query):
-    path = f"https://api.themoviedb.org/3/search/movie?api_key={API_KEY}&query={query}&page=1"
+    path = f"https://api.themoviedb.org/3/search/movie?api_key={TMDB_API_KEY}&query={query}&page=1"
     movieid = requests.get(path).json()
     newid = movieid["results"][0]["id"]
     return newid
+
+    # ratingJSON[imdbJSON["Ratings"][0]["Source"]] = ratings[0]["Value"]
+    # ratingJSON[imdbJSON["Ratings"][1]["Source"]] = ratings[[1]["Value"]
+    # ratingJSON[imdbJSON["Ratings"][2]["Source"]] = ratings[2]["Value"]
+
+
+def getRatings(id):
+    path = f"http://www.omdbapi.com/?apikey={OMDB_API_KEY}&i={id}"
+    imdbJSON = requests.get(path).json()
+    ratingDict = imdbJSON["Ratings"]
+    return ratingDict
 
 
 if __name__ == "__main__":
